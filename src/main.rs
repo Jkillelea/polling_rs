5d54bebd8c4bf37c5b781e8f0beff23fcb1f6c95
Jacob Killelea <jkillelea@protonmail.ch>
2021-10-30 09:54:25 -0500

    Refactor a little

--- src/main.rs
+++ src/main.rs
@@ -19,32 +19,37 @@ extern fn sigpoll_callback(signal: c_int) {
     }
 }
 
+unsafe fn register_sigpoll(file: &File, callback: unsafe extern "C" fn(i32)) {
+    use imports::*;
+
+    let fd = file.as_raw_fd();
+    fcntl(fd as c_int, F_SETOWN as i32, getpid());
+    fcntl(fd as c_int, F_SETFL  as i32, O_NONBLOCK | O_ASYNC);
+
+    let sa = sigaction {
+        __sigaction_handler: sigaction__bindgen_ty_1 {
+            sa_handler: Some(callback),
+        },
+
+        sa_flags: 0,
+
+        sa_mask: __sigset_t {
+            __val: [0; mem::size_of::<__sigset_t>() / mem::size_of::<c_ulong>()]
+        },
+
+        sa_restorer: None,
+    };
+
+    let mut oldsa = mem::zeroed();
+    sigaction(SIGIO as i32, &sa, &mut oldsa);
+}
+
 fn main() -> io::Result<()> {
     let fname = env::args().nth(1).unwrap_or("/dev/ttyS0".into());
     let f     = File::open(fname)?;
-    let fd    = f.as_raw_fd();
 
     unsafe {
-        use imports::*;
-        fcntl(fd as c_int, F_SETOWN as i32, getpid());
-        fcntl(fd as c_int, F_SETFL as i32, O_NONBLOCK | O_ASYNC);
-
-        let sa = sigaction {
-            __sigaction_handler: sigaction__bindgen_ty_1 {
-                sa_handler: Some(sigpoll_callback),
-            },
-
-            sa_flags: 0,
-
-            sa_mask: __sigset_t {
-                __val: [0; mem::size_of::<__sigset_t>() / mem::size_of::<c_ulong>()]
-            },
-
-            sa_restorer: None,
-        };
-
-        let mut oldsa = mem::zeroed();
-        sigaction(SIGIO as i32, &sa, &mut oldsa);
+        register_sigpoll(&f, sigpoll_callback);
     }
 
     // Put the main thread to sleep and wait for signals
